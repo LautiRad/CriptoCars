6481a46c3001904d973b1391ff299f04b08934e9
Braifz <braian94lp@gmail.com>
2022-09-02 20:09:42 -0300

    validate form with Yup and add previewImage component

--- components/formulario.js
+++ components/formulario.js
@@ -1,33 +1,13 @@
 import React, { useState } from "react";
-import { Formik, Field, ErrorMessage } from "formik";
+import { Formik, Field, Form } from "formik";
 import axios from "axios";
+import * as Yup from "yup";
+import PreviewImage from "./PreviewImage";
 import styles from "../styles/Home.module.css";
 
 export default function Formulario() {
   const [formSend, setFormSend] = useState(false);
-
-  // const postForm = async (values) => {
-  //   const options = {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization:
-  //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjYyMDU5Nzc0LCJleHAiOjE2NjI2NjQ1NzR9.4nQNTOISXVSM54C1no7XD8-aEV47PayLzLQ5Lz3zjbs",
-  //       "Content-Type": "application/json",
-  //     },
-  //     data: values,
-  //   };
-
-  //   return fetch("https://criptocar-api.azurewebsites.net/api/v1/post", options)
-  //     .then((response) => response.json())
-  //     .then((res) => {
-  //       if (res?.status === 200) {
-  //         return res;
-  //       }
-  //       throw new Error("Network response was not ok");
-  //     })
-
-  //     .catch((err) => console.log("ERROR", err));
-  // };
+  const [PrevImg, setPrevImg] = useState(null);
 
   const sendData = async (data) => {
     try {
@@ -36,8 +16,7 @@ export default function Formulario() {
         url: "https://criptocar-api.azurewebsites.net/api/v1/post",
         data: data,
         headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjYyMDU5Nzc0LCJleHAiOjE2NjI2NjQ1NzR9.4nQNTOISXVSM54C1no7XD8-aEV47PayLzLQ5Lz3zjbs",
+          Authorization: process.env.NEXT_PUBLIC_BEARER_TOKEN,
           "Content-Type": "application/json",
         },
       });
@@ -48,7 +27,6 @@ export default function Formulario() {
   };
 
   const initialValues = {
-    id: "",
     nameCar: "",
     description: "",
     model: "",
@@ -56,82 +34,68 @@ export default function Formulario() {
     price: "",
     ubication: "",
     email: "",
-    img: "",
+    image: "",
     message: "",
   };
 
-  const validate = (value) => {
-    let errors = {};
-    {
-      // Validacion nombre
-      if (!value.nameCar) {
-        errors.nameCar = "Por favor ingresa un nombre";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.nameCar)) {
-        errors.nameCar = "El nombre solo puede contener letras y espacios";
-      }
-      // Validacion marca
-      if (!value.model) {
-        errors.model = "Por favor ingresa una marca";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.model)) {
-        errors.model = "El nombre solo puede contener letras y espacios";
-      }
-      // Validacion descripcion
-      if (!value.descripcion) {
-        errors.descripcion = "Por favor ingresa una descripcion";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.descripcion)) {
-        errors.descripcion =
-          "La descripcion solo puede contener letras y espacios";
-      }
-      // Validacion model
-      if (!value.model) {
-        errors.model = "Por favor ingresa un modelo";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.model)) {
-        errors.model = "El Año del modelo solo puede contener numeros";
-      }
-      // Validacion km
-      if (!value.km) {
-        errors.km = "Por favor ingresa un km";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.km)) {
-        errors.km = "El nombre solo puede contener letras y espacios";
-      }
-
-      // Validacion precio
-      if (!value.precio) {
-        errors.precio = "Por favor ingresa un precio";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.precio)) {
-        errors.precio = "El nombre solo puede contener letras y espacios";
-      }
-      // Validacion ubicacion
-      if (!value.ubicacion) {
-        errors.ubicacion = "Por favor ingresa una ubicación";
-      } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(value.ubicacion)) {
-        errors.ubicacion = "El nombre solo puede contener letras y espacios";
-      }
-      // Validacion correo
-      if (!value.correo) {
-        errors.correo = "Por favor ingresa un correo electronico";
-      } else if (
-        !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value.correo)
-      ) {
-        errors.correo =
-          "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.";
-      }
-    }
-  };
+  const validate = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Nombre del modelo muy corto!")
+      .max(50, "Nombre del modelo muy largo!")
+      .required("Campo requerido!"),
+    description: Yup.string()
+      .min(10, "Descripción muy corta!")
+      .max(60, "Descripción muy larga")
+      .required("Campo requerido"),
+    model: Yup.number("Tiene que ser un numero")
+      .required("Campo requerido")
+      .positive()
+      .integer(),
+    km: Yup.number("Tiene que ser un numero")
+      .required("Campo requerido")
+      .positive()
+      .integer(),
+    price: Yup.number("Tiene que ser un numero")
+      .required("Campo requerido")
+      .positive()
+      .integer(),
+    ubication: Yup.string()
+      .min(10, "Descripción muy corta!")
+      .max(60, "Descripción muy larga")
+      .required("Campo requerido"),
+    email: Yup.string()
+      .min(10, "Descripción muy corta!")
+      .max(60, "Descripción muy larga")
+      .required("Campo requerido"),
+    image: Yup.mixed()
+      .required("Campo requerido")
+      .test(
+        "FILE_SIZE",
+        "Muy grande la imagen",
+        (value) => value && value.size < 1024 * 1024
+      )
+      .test(
+        "FILE_TYPE",
+        "Invalido",
+        (value) => value && ["image/png", "image/jpeg"].includes(value.type)
+      ),
+    message: Yup.string()
+      .min(10, "Descripción muy corta!")
+      .max(60, "Descripción muy larga")
+      .required("Campo requerido"),
+  });
 
   const handleSubmit = async (value) => {
     const data = {
-      nameCar: value.nameCar,
+      name: value.name,
       description: value.description,
       model: value.model,
       km: value.km,
       price: value.price,
       ubication: value.ubication,
       email: value.email,
-      img: value.img,
-      user: {
-        id: value.id,
-      },
+      image: value.image,
+      message: value.message,
     };
     console.log("Formulario enviado", data);
     sendData(data);
@@ -143,25 +107,22 @@ export default function Formulario() {
     <>
       <Formik
         initialValues={initialValues}
-        validate={validate}
+        validationSchema={validate}
         onSubmit={handleSubmit}
       >
-        {({ errors, handleSubmit, setFieldValue }) => (
-          <form onSubmit={handleSubmit} className={styles.formulario}>
+        {({ errors, touched, setFieldValue }) => (
+          <Form className={styles.formulario}>
             <div>
               <label htmlFor="nameCar">Marca y Modelo:</label>
               <Field
                 type="text"
-                id="nameCar"
-                name="nameCar"
+                id="name"
+                name="name"
                 placeholder="Chevrolet S10"
               />
-              <ErrorMessage
-                name="nameCar"
-                component={() => (
-                  <div className={styles.error}>{errors.marca}</div>
-                )}
-              />
+              {errors.name && touched.name ? (
+                <div className={styles.error}>{errors.name}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="description">Breve Descripción: </label>
@@ -171,42 +132,30 @@ export default function Formulario() {
                 name="description"
                 placeholder="2.8 LS 4x4 CD 16V Turbo Diesel 4P Manual."
               />
-              <ErrorMessage
-                name="descripcion"
-                component={() => (
-                  <div className={styles.error}>{errors.descripcion}</div>
-                )}
-              />
+              {errors.description && touched.description ? (
+                <div className={styles.error}>{errors.description}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="">Año: </label>
               <Field type="number" id="model" name="model" placeholder="2021" />
-              <ErrorMessage
-                name="model"
-                component={() => (
-                  <div className={styles.error}>{errors.model}</div>
-                )}
-              />
+              {errors.model && touched.model ? (
+                <div className={styles.error}>{errors.model}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="km">Kilometraje: </label>
               <Field type="number" id="km" name="km" placeholder="35000" />
-              <ErrorMessage
-                name="km"
-                component={() => (
-                  <div className={styles.error}>{errors.km}</div>
-                )}
-              />
+              {errors.km && touched.km ? (
+                <div className={styles.error}>{errors.km}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="price">Precio: (Expresado en USDT) </label>
               <Field type="text" id="price" name="price" placeholder="21500" />
-              <ErrorMessage
-                name="precio"
-                component={() => (
-                  <div className={styles.error}>{errors.precio}</div>
-                )}
-              />
+              {errors.price && touched.price ? (
+                <div className={styles.error}>{errors.price}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="ubication">Ubicación: </label>
@@ -216,12 +165,9 @@ export default function Formulario() {
                 name="ubication"
                 placeholder="El Chaltén, Santa Cruz, Argentina."
               />
-              <ErrorMessage
-                name="ubicacion"
-                component={() => (
-                  <div className={styles.error}>{errors.ubicacion}</div>
-                )}
-              />
+              {errors.ubication && touched.ubication ? (
+                <div className={styles.error}>{errors.ubication}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="email">Mail de contacto: </label>
@@ -231,24 +177,26 @@ export default function Formulario() {
                 name="email"
                 placeholder="micorreo@criptocars.com"
               />
-              <ErrorMessage
-                name="correo"
-                component={() => (
-                  <div className={styles.error}>{errors.correo}</div>
-                )}
-              />
+              {errors.email && touched.email ? (
+                <div className={styles.error}>{errors.email}</div>
+              ) : null}
             </div>
             <div>
               <label htmlFor="img">Imagen: </label>
               <input
                 type="file"
                 id="img"
-                name="img"
+                name="image"
                 accept="image/*"
                 onChange={(event) => {
-                  setFieldValue("img", event.currentTarget.files[0]);
+                  setPrevImg(event.currentTarget.files[0]);
+                  setFieldValue("image", event.currentTarget.files[0]);
                 }}
               />
+              {errors.image && touched.image ? (
+                <div className={styles.error}>{errors.image}</div>
+              ) : null}
+              {PrevImg != null && <PreviewImage file={PrevImg} />}
             </div>
             <div>
               <Field
@@ -256,13 +204,16 @@ export default function Formulario() {
                 as="textarea"
                 placeholder="Mensaje o comentario que quieras agregar.."
               />
+              {errors.message && touched.message ? (
+                <div className={styles.error}>{errors.message}</div>
+              ) : null}
             </div>
 
             <button type="submit">Enviar</button>
             {formSend && (
               <p className={styles.exito}>Formulario enviado con exito!</p>
             )}
-          </form>
+          </Form>
         )}
       </Formik>
     </>
