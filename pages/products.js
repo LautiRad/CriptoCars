a9746960bc18cf430b0596bb2dddcc024069b591
Matiaspp96 <matiaas.p@gmail.com>
2022-09-16 20:43:10 -0300

    fix:(Enviroments vars) - feat:(fetch Data/Products

--- pages/products.js
+++ pages/products.js
@@ -1,160 +1,93 @@
-import React , {useState , useEffect} from 'react'
-import AppBarCC from '../components/ResponsiveAppBar/AppBarCC'
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import Logo from '../public/images/full.png'
-import Image from 'next/image'
-import AutitoProd from '../public/images/autitoProd.png'
-import iconUSDT from '../public/images/iconUSDT.png'
-import Button from '@mui/material/Button'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import ProductCard from '../components/Articles/ProductCard'
+import React, { useState, useEffect } from "react";
+import AppBarCC from "../components/ResponsiveAppBar/AppBarCC";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/ProductPage.module.css";
+import Logo from "../public/images/full.png";
+import Image from "next/image";
+import AutitoProd from "../public/images/autitoProd.png";
+import iconUSDT from "../public/images/iconUSDT.png";
+import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ProductCard from "../components/Articles/ProductCard";
+import axios from "axios";
+import { Grid } from "@mui/material";
 
 export default function Products() {
   const theme = createTheme({
     palette: {
-      type: 'light',
+      type: "light",
       primary: {
-        main: '#ED1848',
+        main: "#ED1848",
       },
       secondary: {
-        main: '#f50057',
+        main: "#f50057",
       },
     },
   });
-  
-  const [data, setData] = useState(null)
 
-  const fetchProducts = async() => {
-    let myHeaders = new Headers({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjYyMDU5NDU3LCJleHAiOjE2NjI2NjQyNTd9.HMTPnA0QuAQNXBHnUhiADOQzhFKwH11R738h1SOM8Zo'
-      
-  });
-    
-  
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get(
+          "https://criptocar-api.azurewebsites.net/api/v1/post",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjYzMTcyMzc2LCJleHAiOjE2NjM3NzcxNzZ9.S6OoWeogiXvTehCh2s1obih9x21sEZF1ML9rwwdxwzs",
+            },
+          }
+        );
+        setData(res.data.message);
+      } catch (error) {
+        console.log(error);
+      }
     };
-  
-    const res = await fetch(
-      'URL_GET', 'https://criptocar-api.azurewebsites.net/api/v1/post',
-      requestOptions,
-    )
-      .then(response => response.json())
-      .then(result => {
-        setData(result.message);
-        return result;
-      })
-      .catch(error => console.log('error', JSON.stringify(error)));
-    return res;
-  };
-  
-  useEffect(()=>{
-    fetchProducts()
-    },[])
+    fetchProducts();
+  }, []);
 
   console.log(data);
-    return (
-      <ThemeProvider theme={theme}>
-        <div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
         <Head>
           <title>CriptoCars</title>
           <meta name="description" content="Versión beta de criptocars" />
           <link rel="icon" href="/icon.ico" />
         </Head>
-        <AppBarCC/>
-        
-        <ProductCard/>
-        <div className={styles.grid}>
-        {data && data.map((element)=>{
-                  return (
-                    <div key={element} className={styles.containerC}>
-                      <div className={styles.imagewrapperC}>
-                        <Image
-                          className={styles.imageC}
-                          src={AutitoProd}
-                          alt="Autito"
-                        />
-                      </div>
-                      <div className={styles.contentC}>
-                        <div className={styles.titlewrapperC}>
-                          <div className={styles.leftC}>
-                            <Image
-                              className={styles.iconC}
-                              src={iconTool}
-                              alt="icon tool"
-                            />
-                            <div className={styles.productnameC}>
-                              <p>Fiat 500</p>
-                              <p>{element.name}</p> */
-                            </div>
-                          </div>
-                          <Image
-                            className={styles.iconC}
-                            src={Heart}
-                            alt="icon heart"
-                          />
-                        </div>
-                        <div className={styles.descriptionC}>
-                          <p className={styles.descriptiontextC}>
-                            1.4 Cult 8v. Nafta. 2P Automático
-                          </p>
-                          <p>{element.descriptionC}</p> */
-                        </div>
-                        <div className={styles.priceC}>
-                          <Image
-                            className={styles.iconusdtC}
-                            src={iconUSDT}
-                            alt="icon usdt"
-                          />
-                          <p className={styles.pricecurrencyC}>USDT</p>
-                          <p className={styles.priceamountC}>5.000</p>
-                          <p>{element.price}</p>
-                        </div>
-                        <div className={styles.specificationsC}>
-                          <p className={styles.specificationyearC}>2012</p>{" "}
-                          <p>{element.year}</p>
-                          <p className={styles.specificationdistanceC}>
-                            92.000 km
-                          </p>{" "}
-                          <p>{element.distance}</p> */
-                        </div>
-                        <div className={styles.locationC}>
-                          <Image
-                            src={MapMaker}
-                            className={styles.locationiconC}
-                            alt="map marker icon"
-                          />
-                          <p className={styles.locationtextC}>
-                            Buenos Aires, Argentina
-                          </p>{" "}
-                          <p>{element.ubication}</p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-      })} 
-        </div>
-        
-          <div className={styles.container}>
-            <footer className={styles.footer}>
-              <Link href="https://www.instagram.com/criptocars/"><a target="_blank" rel="noopener noreferrer">
-              <Image src={Logo} alt="Logo" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}/>
+        <AppBarCC />
+
+        {/* <ProductCard /> */}
+        <Grid container spacing={2}>
+          {data &&
+            data.map((element) => {
+              return <ProductCard key={element.id} {...element} />;
+            })}
+        </Grid>
+
+        <div className={styles.container}>
+          <footer className={styles.footer}>
+            <Link href="https://www.instagram.com/criptocars/">
+              <a target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={Logo}
+                  alt="Logo"
+                  sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+                />
               </a>
-              </Link>
-            </footer>
-          </div>
+            </Link>
+          </footer>
         </div>
-      </ThemeProvider>
-    )
-  }
+      </div>
+    </ThemeProvider>
+  );
+}
 
-
-
-   {/*           DEJO ACÁ COMENTADA LA CARD QUE ESTA LIGADA AL GET
+{
+  /*           DEJO ACÁ COMENTADA LA CARD QUE ESTA LIGADA AL GET
 
               <div className={styles.grid}>
                 {data && data.map((element)=>{
@@ -215,4 +148,5 @@ export default function Products() {
                 </div>
             )
                 
-          })}*/}
+          })}*/
+}
--- pages/sell.js
+++ pages/sell.js
@@ -1,54 +1,62 @@
-import React , { useState , useEffect } from 'react'
-import AppBarCC from '../components/ResponsiveAppBar/AppBarCC'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Logo from '../public/images/full.png'
-import Image from 'next/image'
-import Formulario from '../components/formulario';
+import React, { useState, useEffect } from "react";
+import AppBarCC from "../components/ResponsiveAppBar/AppBarCC";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Logo from "../public/images/full.png";
+import Image from "next/image";
+import Formulario from "../components/formulario";
 
 export default function Sell() {
-  
+  return (
+    <div>
+      <Head>
+        <title>CriptoCars</title>
+        <meta name="description" content="Versión beta de criptocars" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <AppBarCC />
+      <br></br>
 
-    return (
-      <div>
-        <Head>
-          <title>CriptoCars</title>
-          <meta name="description" content="Versión beta de criptocars" />
-          <link rel="icon" href="/icon.png" />
-        </Head>
-        <AppBarCC/>
-        <br></br>
-
-        
-        <div className={styles.grid}>
+      <div className={styles.grid}>
         <div className={styles.sell_form}>
           <a className={styles.card_form}>
             <h2>Formulario de Venta</h2>
-              <br/>
-              <p>
-              Para publicar tu vehículo en criptocars tendrás que completar y enviar el siguiente formulario.
-              </p>
-              <br/>
-              <p>
-              Tené en cuenta dejar tu mail de contacto de manera correcta asi nos podemos contactar con vos.
-              </p>
-              <br/>
-              <p>Completa el siguiente formulario para poder publicar tu vehículo, si todo está en orden nos comunicaremos con vós y procederemos a publicar tu vehículo.
-              </p>
-            <Formulario/>
+            <br />
+            <p>
+              Para publicar tu vehículo en criptocars tendrás que completar y
+              enviar el siguiente formulario.
+            </p>
+            <br />
+            <p>
+              Tené en cuenta dejar tu mail de contacto de manera correcta asi
+              nos podemos contactar con vos.
+            </p>
+            <br />
+            <p>
+              Completa el siguiente formulario para poder publicar tu vehículo,
+              si todo está en orden nos comunicaremos con vós y procederemos a
+              publicar tu vehículo.
+            </p>
+            <Formulario />
           </a>
         </div>
-        </div>
-        
-
-          <div className={styles.container}>
-            <footer className={styles.footer}>
-              <a href="https://www.instagram.com/criptocars/" target="_blank" rel="noopener noreferrer">
-              <Image src={Logo} alt="Logo" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}/>
-              </a>
-            </footer>
-          </div>
-        
       </div>
-    )
-  }
+
+      <div className={styles.container}>
+        <footer className={styles.footer}>
+          <a
+            href="https://www.instagram.com/criptocars/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={Logo}
+              alt="Logo"
+              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+            />
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
