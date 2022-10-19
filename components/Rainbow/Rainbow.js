2337a4d4147bd3a4bb3114a1a31dad581c0f3c87
LautiRad <lautaroradboone@gmail.com>
2022-10-18 21:40:39 -0300

    Rainbow

--- components/Rainbow/Rainbow.js
+++ components/Rainbow/Rainbow.js
@@ -1,16 +1,13 @@
-import "@rainbow-me/rainbowkit/styles.css";
+import "@rainbow-me/rainbowkit/styles.css"
 import {
   ConnectButton,
   getDefaultWallets,
   darkTheme,
-  RainbowKitProvider, 
-  Theme
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-import merge from 'lodash.merge';
-
+  RainbowKitProvider
+} from "@rainbow-me/rainbowkit"
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi"
+import { alchemyProvider } from "wagmi/providers/alchemy"
+import { publicProvider } from "wagmi/providers/public"
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
@@ -27,7 +24,6 @@ const wagmiClient = createClient({
   connectors,
   provider
 });
-
 
 export default function IndexPage() {
   return (
