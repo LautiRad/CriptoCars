df33d1194f3ec93d070bf0981664417344e37a67
Martín <moreiragmartin@gmail.com>
2023-02-27 15:36:36 -0300

    Se agrega header en backoffice

--- pages/backoffice.js
+++ pages/backoffice.js
@@ -1,11 +1,14 @@
 import { Container, Stack, Typography } from '@mui/material';
 import CarList from '../components/Backoffice/Cars/CarList.js';
+import Header from '../components/Backoffice/header/Header.js';
+import * as React from "react";
 
 export default function CarPage() {
     return (
         <>
-            <Container>
-                <Typography variant="h4" sx={{ mb: 5 }}>
+            <Header />
+            <Container sx={{ display: { xs: "block", md: "block"}}}>
+                <Typography variant="h4" sx={{ mb: 5 , mt: 5    }}>
                     Cars
                 </Typography>
                 <CarList />
--- pages/backoffice/[id].js
+++ pages/backoffice/[id].js
@@ -1,9 +1,12 @@
 import { Container } from '@mui/material';
 import CarDetail from '../../components/Backoffice/Cars/CarDetail.js';
+import Header from '../../components/Backoffice/Header/Header.js';
+
 
 export default function Car() {
     return (
         <>
+            <Header/>
             <Container>
                 <CarDetail/>
             </Container>
