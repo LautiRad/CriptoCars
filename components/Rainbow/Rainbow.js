e759f5db295ed08f5ed6273d6e2d368aa6211c4d
LautiRad <lautaroradboone@gmail.com>
2023-03-03 15:09:17 -0300

    update rainbow 🚀

--- components/Rainbow/Rainbow.js
+++ components/Rainbow/Rainbow.js
@@ -1,49 +1,17 @@
-import "@rainbow-me/rainbowkit/styles.css"
-import {
-  ConnectButton,
-  getDefaultWallets,
-  darkTheme,
-  RainbowKitProvider
-} from "@rainbow-me/rainbowkit"
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi"
-import { alchemyProvider } from "wagmi/providers/alchemy"
-import { publicProvider } from "wagmi/providers/public"
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider
-});
-
-export default function IndexPage() {
+export default function Rainbow() {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme({
-        accentColor: '#1C2A3C',
-        accentColorForeground: 'white',
-      })}>
-        <div
-          style={{
-            marginLeft: '10px',
-            height: "10vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "end",
-          }}
-        >
-          <ConnectButton label="Entrar" />
-        </div>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <div
+      style={{
+        marginLeft: "10px",
+        height: "10vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "end",
+      }}
+    >
+      <ConnectButton label="Entrar" />
+    </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,45 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return (<Component {...pageProps} />
-  )
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme({
+          accentColor: "#1C2A3C",
+          accentColorForeground: "white",
+        })}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
