1deaf06ae916a09fd6344ec5b5e25056f6fd4a8a
Martín <moreiragmartin@gmail.com>
2023-03-07 16:59:04 -0300

    Se agrega loading y refresh de sell

--- components/Loading/LoadingSpinner.js
+++ components/Loading/LoadingSpinner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../../../styles/Loading.module.css'
+import styles from '../../styles/Loading.module.css'
 
 export default function LoadingSpinner() {
     return (
